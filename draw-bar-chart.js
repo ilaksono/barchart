4e6acc266f0fce75891f52857d97504f1a5b15e0
ilaksono <ian.laksono@gmail.com>
2020-09-13 22:57:10 +0000

    bar value function

--- draw-bar-chart.js
+++ draw-bar-chart.js
@@ -18,9 +18,37 @@ function changeSize (val1, val2, element) {
 
 function plotData(data) {
   let num = data.length;
+  createGrid(num);
   xAxisLabels(num);
 }
 
 function xAxisLabels(num) {
-  d
 }
+
+function createGrid(num) {
+  // document.getElementById('graph').style['grid-template-columns'] = 'repeat(' + num + ', 1fr)';
+  for(let i = 0; i < num; i++) {
+    $('#graph').append($('<div id="bar' + i +'" class="graph-bar"></div>'));
+    console.log('bar' + i);
+  } 
+}
+
+function barValues(arr) {
+  let max = arr[0];
+  for(let a of arr) {
+    if(max < a)
+      max = a;
+  }
+
+  for(let i = 0; i < arr.length; i++) {
+    let temp = document.getElementById('bar' + i);
+    temp.createAttribute('width');
+    temp.style.width = arr[i] / max;
+  }
+
+}
+
+const arr = [1,2,3,4,5,6,7,8,9,10]
+
+createGrid(10);
+barValues(arr)
